a66eaafd8b1c728ced8c75056a1b11b894ae38a7
Ravi Shetye <ravi@xplain.io>
2019-03-23 18:07:38 -0700

    DEBUG TO WARN

--- src/main/java/com/mitdbg/modeldb/experimentRun/ExperimentRunServiceImpl.java
+++ src/main/java/com/mitdbg/modeldb/experimentRun/ExperimentRunServiceImpl.java
@@ -734,12 +734,12 @@ public class ExperimentRunServiceImpl extends ExperimentRunServiceImplBase {
         throw StatusProto.toStatusRuntimeException(status);
       }
 
-      LOGGER.log(Level.DEBUG, "in log metric");
+      LOGGER.log(Level.WARNING, "in log metric");
       List<KeyValue> metricList = experimentRunDAO.getExperimentRunMetrics(request.getId());
-      LOGGER.log(Level.DEBUG, metricList);
+      LOGGER.log(Level.WARNING, metricList);
       for (KeyValue metric : metricList) {
           if (metric.getKey() == request.getMetric().getKey()) {
-              LOGGER.log(Level.DEBUG, "Found matching metric");
+              LOGGER.log(Level.WARNING, "Found matching metric");
               Status status =
                   Status.newBuilder()
                       .setCode(Code.ALREADY_EXISTS.getNumber())
@@ -755,7 +755,7 @@ public class ExperimentRunServiceImpl extends ExperimentRunServiceImplBase {
       responseObserver.onNext(
           LogMetric.Response.newBuilder().setExperimentRun(updatedExperimentRun).build());
       responseObserver.onCompleted();
-      LOGGER.log(Level.DEBUG, "successfully wrote metric" + request.getMetric().getKey());
+      LOGGER.log(Level.WARNING, "successfully wrote metric" + request.getMetric().getKey());
     } catch (StatusRuntimeException e) {
       LOGGER.log(Level.WARNING, e.getMessage(), e);
       responseObserver.onError(e);
